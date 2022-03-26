5fb9da435e01dfac817e8e02b4a75b400ee9ffb6
letterpaulsand <87470961+letterpaulsand@users.noreply.github.com>
2022-03-26 22:56:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const port = 9000;
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
 
